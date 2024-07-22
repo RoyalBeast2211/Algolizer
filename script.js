ce209a8399540fe549664598819ebd6aa55cce18
Omkar More <theomkarmore@gmail.com>
2024-07-22 15:09:03 +0530

    setting up all files

---
+++ script.js
@@ -0,0 +1,13 @@
+const n = 10;
+const array = [];
+
+for (let i = 0; i < n; i++) {
+  array[i] = Math.random();
+}
+
+for (let i = 0; i < array.length; i++) {
+  const bar = document.createElement("div");
+  bar.style.height = array[i] * 100 + "%";
+  bar.classList.add("bar");
+  container.appendChild(bar);
+}
